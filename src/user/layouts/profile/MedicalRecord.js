638ceae772836fdb661a567d195be8b4284a63e3
diofeher <diofeher@gmail.com>
2017-10-15 08:39:00 -0400

    missing module

---
+++ src/user/layouts/profile/MedicalRecord.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+
+class MedicalRecord extends Component {
+  constructor(props, { authData }) {
+    super(props)
+    this.state = {
+      data: '',
+      record: {
+        name: '',
+        height: '',
+        weight: '',
+        allergies: []}
+    };
+    console.log('called', this.state);
+  }
+
+  render() {
+    return(
+      <main className="container">
+        <div className="pure-g">
+          <div className="pure-u-1-1">
+            <h1>Medical Record</h1>
+            <div className="table table-responsive">
+              <p><strong>Name  </strong>{ this.props.record ? this.props.record.name : '' }</p>
+              <p><strong>Height  </strong>{ this.props.record ? this.props.record.height : '' }</p>
+
+              <p><strong>Weight  </strong>{ this.props.record ? this.props.record.weight : '' }</p>
+              <p>
+                <strong>Allergies</strong>
+                <ul>
+                {this.props.record.allergies.length > 0 &&
+                   this.props.record.allergies.map((x, i) =>
+                      <li>{i}</li>
+                    )
+                }
+                </ul>
+              </p>
+            </div>
+          </div>
+        </div>
+    </main>
+    )
+  }
+}
+
+export default MedicalRecord
